6d8a397132910c13582242fc3b26edc8860a3476
Dzmitry Anishchuk <35065595+fanbee@users.noreply.github.com>
2020-10-02 22:48:29 +0300

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) {    
+    if (typeof array != "undefined" && array != null && array.length > 0){
+        let min_value=array[0];
+        for(var i = 0 ; i<array.length; i++){
+            if(min_value > array[i]) {min_value=array[i]}
+        }
+        return min_value;
+    }
+    else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof array != "undefined" && array != null && array.length > 0){
+        let max_value=array[0];
+        for(var i = 0 ; i<array.length; i++){
+            if(max_value < array[i]) {max_value=array[i]}
+        }
+        return max_value;
+      }
+      else {
+        return 0;
+      }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array != "undefined" && array != null && array.length > 0){
+        let avg_value=0;
+        let sum_value=0;
+        for(var i = 0 ; i<array.length; i++){
+            sum_value=sum_value+array[i];            
+        }
+        size_array=array.length;
+        avg_value=sum_value/size_array;
+        return avg_value;
+      }
+      else {
+        return 0;
+      }
 }
